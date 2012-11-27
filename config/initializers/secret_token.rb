f3c22b8e79f5fa2f832cc7d9439120dd98f2d086
jnny <jenny@thepyre.org>
2012-11-26 21:47:42 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SudolibApp::Application.config.secret_token = '716a40c4306579fa275f7b824395b52ec2e5dcb0f9d30da9ff8d8432c919990606ad20ba546f2bc44ad3352ad4884e8d3fafc80361a0f0d6d9df42e41a3500f2'
